5d60ddf6dafa95405399eaf5949c29b2503b4fe9
apfox500 <apfox500@gmail.com>
2022-11-30 17:32:05 -0700

    ui

--- koda/lib/home.dart
+++ koda/lib/home.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
 
-import 'background.dart';
 import 'bottom_buttons.dart';
 import 'global.dart';
 import 'lesson.dart';
@@ -25,71 +24,94 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: Global.davysGrey,
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
+      body: SafeArea(
         child: Center(
-          child: FutureBuilder<Map<Section, bool>>(
-              future: unlocked,
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  List<Section> unlockedSections =
-                      global.masterOrder.keys.toList().where((element) => global.unlocked[element]!).toList();
-                  return ListView.builder(
-                    //Using a listview here to geerate a start for every section that they have unlocked
-                    itemCount: unlockedSections.length,
-                    itemBuilder: ((context, index) {
-                      double progress = 0;
-                      if (global.masterOrder[unlockedSections[index]]!.isNotEmpty) {
-                        progress = (global.currentPlace[unlockedSections[index]]!) /
-                            global.masterOrder[unlockedSections[index]]!.length;
-                      } else {}
+          child: Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(bottom: 10),
+                child: Text(
+                  "KODA",
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontFamily: "SpaceGrotesque",
+                    fontWeight: FontWeight.w900,
+                    letterSpacing: 5,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * .8,
+                width: MediaQuery.of(context).size.width,
+                child: FutureBuilder<Map<Section, bool>>(
+                    future: unlocked,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        List<Section> unlockedSections = global.masterOrder.keys
+                            .toList()
+                            .where((element) => global.unlocked[element]!)
+                            .toList();
+                        return ListView.builder(
+                          //Using a listview here to geerate a start for every section that they have unlocked
+                          itemCount: unlockedSections.length,
+                          itemBuilder: ((context, index) {
+                            double progress = 0;
+                            if (global.masterOrder[unlockedSections[index]]!.isNotEmpty) {
+                              progress = (global.currentPlace[unlockedSections[index]]!) /
+                                  global.masterOrder[unlockedSections[index]]!.length;
+                            } else {}
 
-                      return Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: ListTile(
-                          tileColor: Global.coolGrey,
-                          shape: const RoundedRectangleBorder(
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(
-                                10,
-                              ),
-                            ),
-                          ),
-                          onTap: () {
-                            if (progress < 1) {
-                              int currentPlace = global.currentPlace[unlockedSections[index]]!;
-
-                              Widget page = HomePage(global);
-                              try {
-                                if (global.masterOrder[unlockedSections[index]]![currentPlace] is Question) {
-                                  page = QuestionPage(
-                                      global, global.masterOrder[unlockedSections[index]]![currentPlace]);
-                                } else if (global.masterOrder[unlockedSections[index]]![currentPlace] is Lesson) {
-                                  page = LessonPage(
-                                      global, global.masterOrder[unlockedSections[index]]![currentPlace]);
-                                }
-                              } catch (_) {}
-
-                              Navigator.push(
-                                context,
-                                PageTransition(
-                                  type: PageTransitionType.fade,
-                                  fullscreenDialog: true,
-                                  child: page,
+                            return Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: ListTile(
+                                tileColor: Global.coolGrey,
+                                shape: const RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(
+                                      10,
+                                    ),
+                                  ),
                                 ),
-                              );
-                            }
-                          },
-                          title: Text(global.sectionNames[unlockedSections[index]]!),
-                          subtitle: Text('Progress: ${(progress * 100).round()}%'),
-                        ),
-                      );
+                                onTap: () {
+                                  if (progress < 1) {
+                                    int currentPlace = global.currentPlace[unlockedSections[index]]!;
+
+                                    Widget page = HomePage(global);
+                                    try {
+                                      if (global.masterOrder[unlockedSections[index]]![currentPlace]
+                                          is Question) {
+                                        page = QuestionPage(
+                                            global, global.masterOrder[unlockedSections[index]]![currentPlace]);
+                                      } else if (global.masterOrder[unlockedSections[index]]![currentPlace]
+                                          is Lesson) {
+                                        page = LessonPage(
+                                            global, global.masterOrder[unlockedSections[index]]![currentPlace]);
+                                      }
+                                    } catch (_) {}
+
+                                    Navigator.push(
+                                      context,
+                                      PageTransition(
+                                        type: PageTransitionType.fade,
+                                        fullscreenDialog: true,
+                                        child: page,
+                                      ),
+                                    );
+                                  }
+                                },
+                                title: Text(global.sectionNames[unlockedSections[index]]!),
+                                subtitle: Text('Progress: ${(progress * 100).round()}%'),
+                              ),
+                            );
+                          }),
+                        );
+                      } else {
+                        return const CircularProgressIndicator(); //lil baby loading screen while we get user specific data
+                      }
                     }),
-                  );
-                } else {
-                  return const CircularProgressIndicator(); //lil baby loading screen while we get user specific data
-                }
-              }),
+              ),
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: FooterButtons(global),
