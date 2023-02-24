da61421dd76c84bd4e01b002143969f521b5c532
Rudra Goel <goelrudra2016@gmail.com>
2023-02-23 18:09:48 -0700

    last documentation

--- koda/lib/providers/get_data_from_Python_script.dart
+++ koda/lib/providers/get_data_from_Python_script.dart
@@ -1,29 +1,40 @@
-// ignore_for_file: non_constant_identifier_names
-
+/**
+ * Packages imported
+ */
 import 'dart:convert';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:http/http.dart' as http;
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 Future<ImageData> getDataFromImageAnalyzer(base64String) async {
-  //cant use local server
-  //must pass a url that points to a live, deployed server
-
+  ///Format the data into a Map with key/value pairs
   Map data = {"imageBase64": base64String};
 
+  ///Actually encode the data into JSON format
   final body_ = json.encode(data);
 
-  final resp = await http.post(Uri.parse('https://koda-80dc1-m7mjyb4lxa-uc.a.run.app/analyze'),
-      headers: {"Content-Type": "application/json"}, body: body_);
+  ///Make the HTTP Request
+  ///Method: POST
+  ///The URL points to the service URL of the Googlee Cloud deployed script
+  ///Request pody contains the base64 string
+  final resp = await http.post(
+      Uri.parse('https://koda-80dc1-m7mjyb4lxa-uc.a.run.app/analyze'),
+      headers: {"Content-Type": "application/json"},
+      body: body_);
 
+  ///Takes the response and decodes it into a JSON format. Casts it as a Map with Key/Value pairs
   var decodedResponse = jsonDecode(utf8.decode(resp.bodyBytes)) as Map;
 
+  ///Checks to ensure that the HTTPS request was successful
   if (resp.statusCode == 200) {
+    ///Returns the data --> encoded into a specific format.
+    ///Specific format is outlined below --> Custom Class
     return ImageData.fromJson(decodedResponse);
   } else {
     throw Exception('Failed to process image');
   }
 }
 
+/// Custom classes for processing and organizing data
 class ImageData {
   final String libraries;
   final String VariablesDeclared;
@@ -31,6 +42,7 @@ class ImageData {
   final String PostMessage;
   final List<dynamic> Libs;
 
+  /// Constructor for custom class
   const ImageData(
       {required this.libraries,
       required this.VariablesDeclared,
@@ -38,6 +50,7 @@ class ImageData {
       required this.PostMessage,
       required this.Libs});
 
+  ///Method to load in the data of the response into the custom class
   factory ImageData.fromJson(Map<dynamic, dynamic> json_Data) {
     return ImageData(
         libraries: json_Data["Libraries"],
@@ -48,8 +61,8 @@ class ImageData {
         PostMessage: json_Data["ambussin"]);
   }
 
-  // @override
-
+  ///Method for development/programmers
+  ///Sort of toString method
   dynamic printObj() {
     return {
       'Libraries': libraries,
@@ -61,13 +74,21 @@ class ImageData {
   }
 }
 
-Future<List<Map<String, String>>> getPythonLibraryDescription(List<dynamic> pythLib) async {
+///This is the method for querying the descriptions of all the detected libraries from Recognition
+///It takes in a list of the Python Libraries recognized and returns a Promise of a List which contains Map object
+Future<List<Map<String, String>>> getPythonLibraryDescription(
+    List<dynamic> pythLib) async {
   List<Map<String, String>> ret = [];
 
-  CollectionReference db = FirebaseFirestore.instance.collection("Python Libraries");
+  ///Reference to the collection of the Firestore database which contains all the module descriptions
+  CollectionReference db =
+      FirebaseFirestore.instance.collection("Python Libraries");
 
   for (var e in pythLib) {
-    await db.doc((e as String).toLowerCase()).get().then((DocumentSnapshot doc) {
+    await db
+        .doc((e as String).toLowerCase())
+        .get()
+        .then((DocumentSnapshot doc) {
       if (doc.exists) {
         final data = doc.data() as Map<String, dynamic>;
         ret.add({'Lib': e, "Description": data["Description"]!});
